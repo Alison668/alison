4b4365c8567cbe218480620baa6570cb924cd35f
Alison668 <135245276+Alison668@users.noreply.github.com>
2023-06-01 11:43:32 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import Experience from "./pages/Experience";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/projects" element={<Projects />}></Route>
-          <Route path="/experience" element={<Experience />}></Route>
         </Routes>
       </Router>
     </div>
